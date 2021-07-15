1375770b7826685a3ddbdfc9e1c3ba3d9be25659
navneshranjan <navneshranjan@gmail.com>
2021-07-15 16:32:19 +0530

    [transaction maths]

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -5,7 +5,7 @@ import AppReducer from "./AppReducer";
 const initialState = {
   transactions: [
     { id: 1, text: "Flower", amount: -20 },
-    { id: 2, text: "Salary", amount: 300 },
+    { id: 2, text: "Salary", amount: +600 },
     { id: 3, text: "Book", amount: -10 },
     { id: 4, text: "Camera", amount: 150 },
   ],
--- src/cpmponents/Balance.js
+++ src/cpmponents/Balance.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import { Transaction } from "./Transaction";
 
 export const Balance = () => {
+  const { transactions } = useContext(GlobalContext);
+  const amounts = transactions.map((Transaction) => Transaction.amount);
+  const total = amounts.reduce((acc, item) => acc + item, 0).toFixed(2);
+
   return (
     <div>
       <h4>your Balance</h4>
-      <h1>$0.00</h1>
+      <h1>${total}</h1>
     </div>
   );
 };
